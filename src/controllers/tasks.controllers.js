1d9e5bddacb5f91137aeb0703a0fc3ba95d38379
Bernarth <alberthbernarth75@gmail.com>
2023-04-29 17:16:41 -0400

    Adding middleware handling error

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,15 +1,15 @@
 const pool = require('../db');
 
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (req, res, next) => {
   try {
     const allTasks = await pool.query('SELECT id, title, description FROM task;');
     res.json(allTasks.rows);
   } catch(error) {
-    console.log(error.message);
+    next(error);
   }
 };
 
-const getTask = async (req, res) => {
+const getTask = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
@@ -23,7 +23,7 @@ const getTask = async (req, res) => {
 
     res.json(result.rows[0]);
   } catch(error) {
-    console.log(error.message);
+    next(error);
   }
 };
 
@@ -42,7 +42,7 @@ const createTask = async (req, res, next) => {
   }
 }
 
-const deleteTask = async (req, res) => {
+const deleteTask = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
@@ -55,26 +55,29 @@ const deleteTask = async (req, res) => {
 
     return res.sendStatus(204);
   } catch(error) {
-    console.log(error.message);
+    next(error);
   }
 }
 
 const updateTask = async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
 
-  const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *',
-    [title, description, id]);
+    const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *',
+      [title, description, id]);
 
-  if (result.rowCount === 0) {
-    return res.status(404).json({
-      message: 'Task not found'
-    });
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        message: 'Task not found'
+      });
+    }
+
+    return res.json(result.rows[0]);
+  } catch(error) {
+    next(error);
   }
-
-  return res.json(result.rows[0]);
 }
-
 
 module.exports = {
   getAllTasks,
